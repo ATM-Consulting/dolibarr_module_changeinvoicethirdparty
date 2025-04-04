cbe61d7f43c1d5cbe83e2aec79bc35b01afdbe96
Quentin VIAL--GOUTEYRON <quentin.vial-gouteyron@atm-consulting.fr>
2025-04-04 09:30:51 +0200

    - 🔧 **Amélioration de l'interface :**   - Réduction de la taille du bouton pour une meilleure harmonie visuelle. *1.3.0* – **04/04/2025**   - Repositionnement du bouton en tête de liste des actions, à un emplacement plus logique (anciennement placé après le bouton *Supprimer*). *1.3.0* – **04/04/2025**
    - ➕ **Nouveautés :**
      - Ajout du support pour les **devis fournisseurs**. *1.3.0* – **04/04/2025**
      - Ajout du support pour les **commandes fournisseurs**. *1.3.0* – **04/04/2025**
    
    - 🎨 **Visuel :**
      - Mise à jour de l’icône du module en utilisant une icône **FontAwesome**. *1.3.0* – **04/04/2025**

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,5 +1,20 @@
 # Change Log
 
+## Unreleased
+
+### 🆕 Version *1.3* – **04/04/2025**
+
+- 🔧 **Amélioration de l'interface :**  
+  - Réduction de la taille du bouton pour une meilleure harmonie visuelle. *1.3.0* – **04/04/2025**   
+  - Repositionnement du bouton en tête de liste des actions, à un emplacement plus logique (anciennement placé après le bouton *Supprimer*). *1.3.0* – **04/04/2025**
+
+- ➕ **Nouveautés :**  
+  - Ajout du support pour les **devis fournisseurs**. *1.3.0* – **04/04/2025**
+  - Ajout du support pour les **commandes fournisseurs**. *1.3.0* – **04/04/2025**
+
+- 🎨 **Visuel :**  
+  - Mise à jour de l’icône du module en utilisant une icône **FontAwesome**. *1.3.0* – **04/04/2025**
+
 ## Version 1.2
  - NEW Permet aussi de changer le tiers d'une expédition.
  - Renomme le module dans l'interface
--- core/modules/modchangeinvoicethirdparty.class.php
+++ core/modules/modchangeinvoicethirdparty.class.php
@@ -44,7 +44,7 @@ class modchangeinvoicethirdparty extends DolibarrModules
 
         $this->db = $db;
 
-		$this->editor_name = 'ATM-Consulting';
+		$this->editor_name = 'ATM Consulting';
 		// Id for module (must be unique).
 		// Use here a free id (See in Home -> System information -> Dolibarr for list of used modules id).
 		$this->numero = 104871; // 104000 to 104999 for ATM CONSULTING
@@ -53,13 +53,13 @@ class modchangeinvoicethirdparty extends DolibarrModules
 
 		// Family can be 'crm','financial','hr','projects','products','ecm','technic','other'
 		// It is used to group modules in module setup page
-		$this->family = "other";
+		$this->family = 'ATM Consulting';
 		// Module label (no space allowed), used if translation string 'ModuleXXXName' not found (where XXX is value of numeric property 'numero' of module)
 		$this->name = preg_replace('/^mod/i','',get_class($this));
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
 		$this->description = "Description of module changeinvoicethirdparty";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
-		$this->version = '1.3';
+		$this->version = '1.3.0';
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
 		// Where to store the module in setup page (0=common,1=interface,2=others,3=very specific)
@@ -114,8 +114,8 @@ class modchangeinvoicethirdparty extends DolibarrModules
 		$this->depends = array();		// List of modules id that must be enabled if this module is enabled
 		$this->requiredby = array();	// List of modules id to disable if this one is disabled
 		$this->conflictwith = array();	// List of modules id this module is in conflict with
-		$this->phpmin = array(5,0);					// Minimum version of PHP required by module
-		$this->need_dolibarr_version = array(3,0);	// Minimum version of Dolibarr required by module
+		$this->phpmin = array(7,0);					// Minimum version of PHP required by module
+		$this->need_dolibarr_version = array(16,0);	// Minimum version of Dolibarr required by module
 		$this->langfiles = array("changeinvoicethirdparty@changeinvoicethirdparty");
 
 		// Constants
